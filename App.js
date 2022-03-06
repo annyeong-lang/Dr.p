9b0dcfb9d6024ab314a83db5512278acdc06d65a
unknown <therese86126@ewhain.net>
2022-03-06 20:07:20 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from "react-native-webview";
+import * as Location from "expo-location";
+import { View, Text } from "react-native";
 
 export default function App() {
+  try {
+    Location.requestForegroundPermissionsAsync();
+  } catch (error) {
+    console.log(error);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: "https://gee1suu.github.io/getPosition" }}
+        geolocationEnabled={true}
+        style={{ flex: 1, marginTop: 20, marginRight: 10 }}
+      />
+
+      <View style={{ alignItems: "center" }}>
+        <Text style={{ fontSize: 30, marginVertical: 10 }}>메뉴</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
