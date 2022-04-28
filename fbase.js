892c819c5315d07a072781e676a7bed190d534ca
annyeong-lang <flha369@naver.com>
2022-04-28 15:43:37 +0900

    fourO'clock

---
+++ fbase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore} from 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyAQcYiur1456x49u9ijX98j-CZcmbms-wE",
+    authDomain: "drp1-422ee.firebaseapp.com",
+    projectId: "drp1-422ee",
+    storageBucket: "drp1-422ee.appspot.com",
+    messagingSenderId: "138716877444",
+    appId: "1:138716877444:web:0a3f985267df0b3e3fe8d1"
+};
+
+initializeApp(firebaseConfig);
+const dbService = getFirestore()
+export default dbService;
