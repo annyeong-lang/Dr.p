74b80a29af06caf331c347b6fbeceba809aed0a5
annyeong-lang <flha369@naver.com>
2022-05-04 16:39:15 +0900

    Revert "setting 수정"
    This reverts commit 1784d0f24cd205d9f6cdc4c36f7db1441912ab30.

--- screens/settings/setLocation.js
+++ screens/settings/setLocation.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import React from "react"
 import {Button,HStack,Center} from "native-base"
 import * as Location from 'expo-location';
-import { useState } from "react"
+
 function perm(){
     var status=Location.requestForegroundPermissionsAsync();
     if (status !== 'granted') {
@@ -25,10 +25,10 @@ function perm(){
   }
     return (
         <HStack>
-             <Center p="5" ml="3" mr="12" _text={{fontSize:"xl",color:"#600FD4"}}>위치 서비스 사용</Center>
+            <Center p="5" ml="3" mr="12" _text={{fontSize:"xl",color:"#600FD4"}}>위치 서비스 사용</Center>
             <Button onPress={pressed} ml="4" size="lg" mt="3" mb="3"
-             _hover={{bg:"#8A47EB"}}
-             _pressed={{bg:"#8A47EB"}}
+             _hover={{bg:{buttonColor}}}
+             _pressed={{bg:{buttonColor}}}
              bg={buttonColor}
              >{is}</Button>
         </HStack>
--- screens/settings/setting_main.js
+++ screens/settings/setting_main.js
@@ -1,8 +1,8 @@
 
 import React from "react"
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Entypo } from "@expo/vector-icons"
 import { StatusBar } from 'expo-status-bar';
-import {HStack,VStack,NativeBaseProvider,IconButton,Divider,Box} from "native-base"
+import {Icon,VStack,NativeBaseProvider,IconButton,Divider,Box} from "native-base"
 import Permission from "./setLocation"
 import Format from "./initialization"
 
@@ -13,17 +13,11 @@ export default function Setting({navigation}){
     <NativeBaseProvider >
     <StatusBar />
     <Box bg={color} flex={1}>
-    <HStack mt="20" mbw="1">
-    <Center >
-    <IconButton                           
+    <IconButton ml="7" mt="20"_hover={{ bg:{color} }}
+                              _pressed={{ bg:{color} }}
                               onPress={() => navigation.navigate('Home')}
-                              _icon={{
-                                as: MaterialCommunityIcons,
-                                name: "keyboard-backspace",
-                                size : 12,
-                                color : "black"
-
-                              }}></IconButton></Center></HStack>
+    icon={<Icon as={Entypo} name="emoji-happy"
+    ></Icon>}></IconButton>
     <VStack>
          <Divider mb="1" bg="#8A47EB"/>
         <Permission />
