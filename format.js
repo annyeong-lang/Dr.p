182c72da1c25fed9fc1ccc90f2bcee85fe405502
안지수 <80879666+gee1suu@users.noreply.github.com>
2022-05-10 19:34:38 +0900

    format 수정

--- format.js
+++ format.js
@@ -1,24 +1,23 @@
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
-import dbService from "./fbase";
-import { authService } from "./fbase";
+import { dbService, authService } from "./fbase";
 
-function deleting(collection_name) {
+function deleting(collection_name, userId) {
   const querySnapshot = getDocs(collection(dbService, collection_name));
   querySnapshot.then((docs) => {
-    docs.forEach((element) => {
+    docs.forEach(async (element) => {
       var Id = element.id;
       const docRef = doc(dbService, collection_name, Id);
       const docSnap = await getDocs(docRef);
-      if(userId === docSnap.data().userId) {
+      if (userId === docSnap.data().userId) {
         deleteDoc(doc(dbService, collection_name, Id));
         console.log(Id);
       }
     });
   });
 }
-function deleted(){
+function deleted() {
   const userId = authService.currentUser.uid;
-  deleting("result");
-  deleting("todo");
+  deleting("result", userId);
+  deleting("todo", userId);
 }
 export default deleted();
